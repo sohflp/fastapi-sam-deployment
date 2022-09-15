436d3056d97a398df521d9992199d96a30668d76
Felipe Rodrigues <sohflp@gmail.com>
2022-09-15 11:27:05 +1000

    Add Python scripts for FastAPI routes

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from mangum import Mangum
+
+from .api.v1.api import router as api_router
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World!"}
+
+
+app.include_router(api_router, prefix="api/v1")
+handler = Mangum(app)
